1b81a7d57da4001eff850c9a9c63e590a031dc87
Aidan Lao <laoaidan@gmail.com>
2024-03-13 19:53:19 -0300

    Added cards and marquee

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ function machineGun() {
 }
 
 machineGun();
+
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
