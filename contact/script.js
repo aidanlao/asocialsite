6f61b14a389bc6218c6deb9d068c366a7409ccca
Aidan Lao <laoaidan@gmail.com>
2024-03-17 20:04:33 -0300

    Added faq, contact us, and fixed homepage styling

---
+++ contact/script.js
@@ -0,0 +1,7 @@
+const element = document.querySelector('form');
+element.addEventListener('submit', event => {
+  event.preventDefault();
+  // actual logic, e.g. validate the form
+  console.log('Form submission cancelled.');
+  console.log(event);
+});
